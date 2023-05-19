a2e5914f415b45c0c9c039742cb9cd27e932ae50
adarsh-mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2023-05-19 20:38:30 +0530

    🚀  v1.2

--- pages/index.jsx
+++ pages/index.jsx
@@ -33,7 +33,6 @@ export default function Home() {
     }
   };
 
-
   const filterBySkills = (event) => {
     const filter = event.target.value.toUpperCase();
     if (filter === "") {
@@ -81,12 +80,12 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-between p-24 bg-gradient-to-b from-gray-800 to-gray-900 text-white min-h-screen">
-      <h1 className="text-3xl font-bold mb-8">
+    <main className="flex flex-col items-center justify-between p-4 md:p-24 bg-gradient-to-b from-gray-800 to-gray-900 text-white min-h-screen">
+      <h1 className="text-2xl md:text-3xl font-bold mb-4 md:mb-8">
         Bajaj Health Frontend Challenge
       </h1>
-      <div className="search-bar flex items-center mb-8">
-        <label htmlFor="name" className="mr-2">
+      <div className="search-bar flex flex-col md:flex-row items-center mb-4 md:mb-8">
+        <label htmlFor="name" className="mr-2 mb-2 md:mb-0">
           Search by Name:
         </label>
         <input
@@ -94,15 +93,15 @@ export default function Home() {
           id="name"
           onChange={filterByName}
           placeholder="Enter name"
-          className="p-2 bg-gray-700 text-white rounded-md"
+          className="p-2 bg-gray-700 text-white rounded-md mb-2 md:mb-0 md:mr-4"
         />
-        <label htmlFor="skills" className="ml-4 mr-2">
+        <label htmlFor="skills" className="mr-2 mb-2 md:mb-0">
           Filter by Skills:
         </label>
         <select
           id="skills"
           onChange={filterBySkills}
-          className="p-2 bg-gray-700 text-white rounded-md"
+          className="p-2 bg-gray-700 text-white rounded-md mb-2 md:mb-0 md:mr-4"
         >
           <option value="">All</option>
           <option value="JavaScript">JavaScript</option>
@@ -114,13 +113,13 @@ export default function Home() {
           <option value="Manual Testing">Manual Testing</option>
           <option value="SQL">SQL</option>
         </select>
-        <label htmlFor="designation" className="ml-4 mr-2">
+        <label htmlFor="designation" className="mr-2 mb-2 md:mb-0">
           Filter by Designation:
         </label>
         <select
           id="designation"
           onChange={filterByDesignation}
-          className="p-2 bg-gray-700 text-white rounded-md"
+          className="p-2 bg-gray-700 text-white rounded-md mb-2 md:mb-0"
         >
           <option value="">All</option>
           <option value="Developer">Developer</option>
@@ -128,72 +127,73 @@ export default function Home() {
           <option value="Project Manager">Project Manager</option>
         </select>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th className="py-2">Name</th>
-            <th className="py-2">Designation</th>
-            <th className="py-2">Skills</th>
-            <th className="py-2">Projects</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredDevelopers.length > 0 ? (
-            filteredDevelopers.map((developer, index) => (
-              <tr key={index} className="bg-gray-700">
-                <td className="px-6 py-2">
-                  {developer.name ? developer.name : "--"}
-                </td>
-                <td className="px-6 py-2">
-                  {developer.designation ? developer.designation : "--"}
-                </td>
-                <td className="px-6 py-2">
-                  {developer.skills.join(", ")
-                    ? developer.skills.join(", ")
-                    : "--"}
-                </td>
-                <td className="px-6 py-2">
-                  {developer.projects
-                    ? developer.projects.map((project, index) => (
-                        <a
-                          key={index}
-                          href="#"
-                          onClick={(e) => openPopup(e, project)}
-                          className="text-blue-500 hover:underline px-2"
-                        >
-                          {project.name}
-                        </a>
-                      ))
-                    : "--"}
+      <div className="overflow-x-auto max-w-full">
+        <table className="min-w-full">
+          <thead>
+            <tr>
+              <th className="py-2">Name</th>
+              <th className="py-2">Designation</th>
+              <th className="py-2">Skills</th>
+              <th className="py-2">Projects</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredDevelopers.length > 0 ? (
+              filteredDevelopers.map((developer, index) => (
+                <tr key={index} className="bg-gray-700">
+                  <td className="px-6 py-2">
+                    {developer.name ? developer.name : "--"}
+                  </td>
+                  <td className="px-6 py-2">
+                    {developer.designation ? developer.designation : "--"}
+                  </td>
+                  <td className="px-6 py-2">
+                    {developer.skills.join(", ")
+                      ? developer.skills.join(", ")
+                      : "--"}
+                  </td>
+                  <td className="px-6 py-2">
+                    {developer.projects
+                      ? developer.projects.map((project, index) => (
+                          <a
+                            key={index}
+                            href="#"
+                            onClick={(e) => openPopup(e, project)}
+                            className="text-blue-500 hover:underline px-2"
+                          >
+                            {project.name}
+                          </a>
+                        ))
+                      : "--"}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr className="bg-gray-700">
+                <td colSpan="6" className="px-6 py-2">
+                  No developers found
                 </td>
               </tr>
-            ))
-          ) : (
-            <tr className="bg-gray-700">
-              <td colSpan="6" className="px-6 py-2">
-                No developers found
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-      <footer className="text-gray-500 mt-8">
+            )}
+          </tbody>
+        </table>
+      </div>
+      <footer className="text-gray-500 mt-4 md:mt-8">
         &copy; Adarsh Mamgain - 2023
       </footer>
 
       {/* The popup */}
       {selectedProject && (
-        <div
-          id="popup"
-          className="w-[600px] h-[400px] popup fixed bg-gray-400 p-4 rounded shadow"
-        >
-          <div className="popup-content">
-            <div className="mb-3">
-              <h2 className="text-2xl mb-4">{selectedProject.name}</h2>
+        <div className="popup fixed inset-0 flex items-center justify-center bg-gray-400 bg-opacity-50">
+          <div className="w-full max-w-lg bg-white p-4 rounded shadow">
+            <div className="mb-4">
+              <h2 className="text-xl md:text-2xl mb-2">
+                {selectedProject.name}
+              </h2>
               <p>{selectedProject.description}</p>
             </div>
-            <div className="mb-3">
-              <h3 className="text-xl mb-2">Team:</h3>
+            <div className="mb-4">
+              <h3 className="text-lg md:text-xl mb-2">Team:</h3>
               <ul>
                 {selectedProject.team.map((member, index) => (
                   <li key={index}>
@@ -204,8 +204,8 @@ export default function Home() {
                 ))}
               </ul>
             </div>
-            <div className="mb-3">
-              <h3 className="text-xl mb-2">Tasks:</h3>
+            <div className="mb-4">
+              <h3 className="text-lg md:text-xl mb-2">Tasks:</h3>
               <ul>
                 {selectedProject.tasks.map((task, index) => (
                   <li key={index}>
@@ -214,7 +214,7 @@ export default function Home() {
                 ))}
               </ul>
             </div>
-            <div className="mb-3">
+            <div className="mb-2">
               <button className="text-red-500" onClick={closePopup}>
                 Close
               </button>
